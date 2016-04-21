4ee97e9d8c75c6dddbde9c93723b67144a6f6812
张卓 <contrary.zhuo@gmail.com>
2016-04-21 12:23:13 +0800

    fix path

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -5,14 +5,14 @@
 .hero {
   width: 100%;
   height: 400px;
-  background-image: url("/static/hero.jpg");
+  background-image: url("static/hero.jpg");
   background-size: cover;
 }
 @media screen and (min-width:768px) {
   .hero {
     width: 100%;
     height: 100vh;
-    background-image: url("/static/hero@2x.jpg");
+    background-image: url("static/hero@2x.jpg");
   }
 }
 </style>
